cd8763fc7672a719f1dbfcb6169dab86189c76cc
Thomas Christopher <thomas.j.christopher@gmail.com>
2019-08-24 02:51:45 +0100

    Tagging feature completed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+    include CommentsHelper
     def create
         @comment = Comment.new(comment_params)
         @comment.article_id = params[:article_id]
@@ -9,7 +10,5 @@ class CommentsController < ApplicationController
         redirect_to article_path(@comment.article)
     end
 
-    def comment_params
-        params.require(:comment).permit(:author_name, :body)
-    end
+    
 end
